0a7d6eec6d08e6d74a21e543bc84660f47350cb4
Artem Stepanenko <astepanenko@cybercraftinc.com>
2022-01-24 16:53:14 +0200

    move options to constants

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,20 +2,40 @@
 
 import { Command } from 'commander';
 
-const DESCRIPTION = 'Compares two configuration files and shows a difference.';
 const VERSION = '0.0.1';
+const DESCRIPTION = 'Compares two configuration files and shows a difference.';
+
+const format = {
+  flags: '-f, --format <type>',
+  description: 'Output format',
+};
+
+const help = {
+  flags: '-h, --help',
+  description: 'output usage information',
+};
+
+const version = {
+  flags: '-V, --version',
+  description: 'output the version number',
+};
 
 const app = new Command();
 
-app.option('-V, --version', VERSION).option('-h, --help', DESCRIPTION);
+app
+  .option(help.flags, help.description)
+  .option(version.flags, version.description)
+  .option(format.flags, format.description)
+  .description(DESCRIPTION);
+
+app
+  .argument('<firstFile>', 'First file for compare')
+  .argument('<secondFile>', 'Second file for compare');
 
 app.parse(process.argv);
 
 const options = app.opts();
 
-if (options.version) {
-  console.log(version);
-}
-if (options.help) {
-  console.log(app.help());
-}
+if (options.version) console.log(VERSION);
+if (options.format) console.log(format.description);
+if (options.help) console.log(app.help());
