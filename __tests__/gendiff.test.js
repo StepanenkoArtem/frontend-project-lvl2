eddbccc996eb705a8efcbd7da7685420ba37b55f
Artem Stepanenko <astepanenko@cybercraftinc.com>
2022-01-31 19:37:52 +0200

    add tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,18 +1,27 @@
+import { test, expect } from '@jest/globals';
+import genDiff from "../src/gendiff.js";
+import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import genDiff from "../src/gendiff.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-console.log(__filename);
 
 test('true is true', () => {
+  const expected = `{
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
+}`
   expect(
     genDiff(
-      '__fixtures__/first.json',
-      '__fixtures__/second.json',
+      getFixturePath('first.json'),
+      getFixturePath('second.json'),
       )
-  ).toBe(true);
+  ).toBe(expected);
 });
