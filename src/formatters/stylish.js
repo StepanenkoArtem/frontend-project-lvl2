47533287028e8d9030a76d1366ccdfe0c3bc3cfa
Artem Stepanenko <artem@stepanenko.one>
2022-06-05 15:33:06 +0300

    update stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,41 +1,62 @@
-import {ADDED, DELETED, MODIFIED} from "../constants.js";
+import { ADDED, DELETED, MODIFIED, UNCHANGED } from "../constants.js";
 import _ from "lodash";
 
-export default (diff) => {
-  const iter = (node, depth = 1) => {
-    const shift = " ".repeat(depth * 2);
+// Indentation settings
+const FILLER = '.';
+const BACKSHIFT = 2
 
-    const formatLine = (token, key, content) => {
-      if (_.isObject(content)) {
-        return `${shift}${token} ${key}: ${JSON.stringify(content, " ", 4)}`
-      } else {
-        return `${shift}${token} ${key}: ${content}`;
+// Signs
+const ADD = '+'
+const DEL = '-'
+const EMPTY = ' '
+
+export const genView = (diff, space_count = 0) => {
+  const shift = FILLER.repeat(space_count);
+  const endWrapper = `${FILLER.repeat(BACKSHIFT)}}`
+  const lines = [`${shift}{`];
+
+  Object.keys(diff)
+    .map((key) => {
+      const currentNode = diff[key];
+
+      const formatToken = (token) => `  ${token} `
+
+      const addLine = (key, node) => {
+        const { status, before, after } = node;
+        // debugger;
+        switch (status) {
+          case ADDED:
+            lines.push(`${shift}${formatToken(ADD)}${key}: ${formatLine(after)}`);
+            break;
+          case DELETED:
+            lines.push(`${shift}${formatToken(DEL)}${key}: ${formatLine(before)}`);
+            break;
+          case UNCHANGED:
+            lines.push(`${shift}${formatToken(EMPTY)}${key}: ${formatLine(before)}`);
+            break;
+          case MODIFIED:
+            lines.push(`${shift}${formatToken(DEL)}${key}: ${formatLine(before)}`);
+            lines.push(`${shift}${formatToken(ADD)}${key}: ${formatLine(after)}`);
+            break;
+        }
+      };
+
+      const formatLine = (line) => {
+        if (!_.isObject(line)) {
+          return line;
+        }
       }
-    }
 
-    return Object
-      .keys(node)
-      .flatMap((key) => {
-        const lines = [`{\n`];
-        const { status, before, after } = node[key];
-        if (status === DELETED) {
-          lines.push(formatLine("-", key, before));
-        }
-        if (status === ADDED) {
-          lines.push(formatLine("+", key, after));
-        }
-        if (status === MODIFIED) {
-          lines.push(formatLine("-", key, before));
-          lines.push(formatLine("+", key, after));
-        }
-        if (!status) {
-          lines.push(formatLine( " ", key, iter(node[key], depth + 1)));
-          lines.push("\n}");
-        }
-        return lines;
-      })
-      .join('')
-  }
+      if (!_.has(currentNode, 'status')) {
+        lines.push(`${FILLER.repeat(space_count + 4)}${key}: ${genView(currentNode, space_count+4)}`);
+      } else {
+        // console.log( STATUS: $ { currentNode['status'] }`);
+        addLine(key, currentNode);
+      }
+    })
 
-  return iter(diff);
-}
+  lines.push(endWrapper);
+  return lines.join('\n')
+};
+
+export default genView;
