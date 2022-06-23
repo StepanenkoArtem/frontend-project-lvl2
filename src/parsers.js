c6819fa56d2de262ae994357659c220d18e96abc
Artem Stepanenko <artem@stepanenko.one>
2022-06-23 15:07:49 +0300

    halt execution if file format unsupported

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,14 @@
 import * as yaml from 'js-yaml';
 
 export default ({ type, content }) => {
-  const parsers = {
-    json: JSON.parse,
-    yml: yaml.load,
-    yaml: yaml.load,
-  };
-  return parsers[type](content);
+  switch (type) {
+    case 'json':
+      return JSON.parse(content);
+    case 'yaml':
+    case 'yml':
+      return yaml.load(content);
+    default:
+      console.error('Unsupported file format');
+      process.exit(1);
+  }
 };
