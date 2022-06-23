26b0ecfde298d2572fd66508c030e0fbd8defe07
Artem Stepanenko <artem@stepanenko.one>
2022-06-23 10:58:24 +0300

    remove console error

--- src/readers/readFile.js
+++ src/readers/readFile.js
@@ -3,10 +3,5 @@ import { readFileSync } from 'fs';
 
 export default (filepath) => {
   const normalized = path.resolve(process.cwd(), filepath);
-  try {
-    return readFileSync(normalized);
-  } catch (e) {
-    console.log(`Cannot read file "${filepath}"`);
-    throw e;
-  }
+  return readFileSync(normalized);
 };
