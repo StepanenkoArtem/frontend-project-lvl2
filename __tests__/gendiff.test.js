1c886b07a8b904d682389889c6873e9d764e477d
Artem Stepanenko <artem@stepanenko.one>
2022-06-24 13:52:29 +0300

    optimize tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,37 +2,23 @@ import { test, expect } from '@jest/globals';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
-import view from '../src/gendiff.js';
+import gendiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const jsonBefore = getFixturePath('before.json');
-const jsonAfter = getFixturePath('after.json');
-
-const yamlBefore = getFixturePath('before.yml');
-const yamlAfter = getFixturePath('after.yml');
-
-test('stylish formatter', () => {
-  const expectedStylishDiff = readFileSync(getFixturePath('expected/stylish.txt'), 'utf8');
-
-  expect(view(jsonBefore, jsonAfter)).toBe(expectedStylishDiff);
-  expect(view(yamlBefore, yamlAfter)).toBe(expectedStylishDiff);
-});
-
-test('plain formatter', () => {
-  const expectedPlainDiff = readFileSync(getFixturePath('expected/plain.txt'), 'utf8');
-
-  expect(view(jsonBefore, jsonAfter, 'plain')).toBe(expectedPlainDiff);
-  expect(view(yamlBefore, yamlAfter, 'plain')).toBe(expectedPlainDiff);
-});
-
-test('json.txt formatter', () => {
-  /* eslint-disable no-multi-str */
-  const expectedJsonDiff = readFileSync(getFixturePath('expected/json.txt'), 'utf8');
-
-  expect(view(jsonBefore, jsonAfter, 'json')).toBe(expectedJsonDiff);
-  expect(view(yamlBefore, yamlAfter, 'json')).toBe(expectedJsonDiff);
+test.each([
+  ['before.json', 'after.json', 'stylish', 'expected/stylish.txt'],
+  ['before.json', 'after.json', 'plain', 'expected/plain.txt'],
+  ['before.json', 'after.json', 'json', 'expected/json.txt'],
+  ['before.yml', 'after.yml', 'stylish', 'expected/stylish.txt'],
+  ['before.yml', 'after.yml', 'plain', 'expected/plain.txt'],
+  ['before.yml', 'after.yml', 'json', 'expected/json.txt'],
+])('gendiff (%s, %s) with output format %s', (firstFilepath, secondFilepath, outputFormat, expectedFixtureFilepath) => {
+  const first = getFixturePath(firstFilepath);
+  const second = getFixturePath(secondFilepath);
+  const expected = readFileSync(getFixturePath(expectedFixtureFilepath), 'utf-8');
+  expect(gendiff(first, second, outputFormat)).toBe(expected);
 });
